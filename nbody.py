26a77bf93270ab671edf9a7220f33b4bd635037e
Sameer Ansari <elucidation@gmail.com>
2011-05-09 02:09:40 -0400

    Moved function initialization to init function

--- nbody.py
+++ nbody.py
@@ -1,6 +1,7 @@
 
 
 class Body:
-    x,y,z = 0,0,0 # Position
-    vx,vy,vz = 0,0,0 # Velocity
-    mass = 1 # Mass
+    def __init__(self, x=0,y=0,z=0,vx=0,vy=0,vz=0,mass=1):
+        self.x,self.y,self.z = x,y,z # Position
+        self.vx,self.vy,self.vz = vx,vy,vz # Velocity
+        self.mass = mass # Mass
