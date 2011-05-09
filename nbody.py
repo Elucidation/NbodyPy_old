dd57d07b85198a4dd73b29c824658594d86fda74
Sameer Ansari <elucidation@gmail.com>
2011-05-09 02:51:17 -0400

    Added System class

--- nbody.py
+++ nbody.py
@@ -8,7 +8,7 @@ class Body:
         self.vx,self.vy,self.vz = vx,vy,vz # Velocity
         self.mass = mass # Mass
     def __str__(self):
-        return '%s | Pos: (%d,%d,%d) Vel: <%d,%d,%d> Mass: %d' \
+        return '%s \t| Pos: (%d,%d,%d)\tVel: <%d,%d,%d>\tMass: %d' \
                % (self.name, \
                  self.x,self.y,self.z, \
                  self.vx,self.vy,self.vz, \
@@ -24,11 +24,18 @@ class System:
     def createRandomBodies(self,n):
         for i in range(0,n):
             self.bodies << createRandomBody()
-    def createRandomBody(self):
+    def createRandomBody(self): # Not yet random
         k = Body( 0,0,0, \
                   0,0,0, \
                   1 , 'random')
-                  
+    def 
+    def size(self):
+        return len(self.bodies)
+    def __str__(self):
+        str = "%s \t| Number of Bodies: %d\n" % (self.name,self.size())
+        for body in self.bodies:
+            str += body.__str__() + "\n"
+        return str
         
 
 
@@ -41,7 +48,12 @@ class Test:
         b = Body(name='jill',x=-1,mass=2)
         print a
         print b
+    def test3(self):
+        a = Body(name='bob',y=2,mass=3)
+        b = Body(name='jill',x=-1,vy = 1,mass=2)
+        sys = System(bodies=[a, b],name='world')
+        print sys
 
 
 a = Test()
-a.test2()
+a.test3()
